c69bbdb874b0c73f24a4cbb3bdd5b38d57bbdc00
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2024-02-14 20:46:50 +0530

    Create Largest3number.py

---
+++ Largest3number.py
@@ -0,0 +1,19 @@
+print("Enter 3numbers ")
+
+a=int(input("Enter 1st number: "))
+
+b=int(input("Enter 2nd number: "))
+
+c=int(input("Enter 3rd number: "))
+
+if(a>b and a>c):
+
+print(a," is large")
+
+elif(b>a and b>c):
+
+print(b,"is large")
+
+else:
+
+print(c," is large")
